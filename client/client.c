690e46bba5e5e2d393741678da3007cf133c9739
Jackson Guo <guo_jackson@yahoo.ca>
2018-10-29 13:52:14 -0400

    refactored client.c

--- client/client.c
+++ client/client.c
@@ -6,6 +6,10 @@
 
 #include "elp.h"
 
+void stop_thread(pthread_t tid);
+int arg_check(int argc, char **argv);
+int stdin_loop(elpinfo_t *info);
+
 void stop_thread(pthread_t tid)
 {
     pthread_cancel(tid);
@@ -20,6 +24,25 @@ int arg_check(int argc, char **argv)
     } else if (strlen(argv[1]) > INET6_ADDRSTRLEN) {
         fprintf(stderr, "Usage: <ip> is over %d bytes\n", INET6_ADDRSTRLEN);
         return -1;
+    }
+
+    return 0;
+}
+
+int stdin_loop(elpinfo_t *info)
+{
+    char input[512];
+    while (1) {
+        if (!fgets(input, 512, stdin)) {
+            if (ferror(stdin)) {
+                perror("fgets");
+                return 1;
+            }
+
+            break;
+        }
+
+        elp_give_msg(info, input);
     }
 
     return 0;
@@ -43,24 +66,11 @@ int main(int argc, char **argv)
     pthread_t elptid;
     pthread_create(&elptid, NULL, elp_run, &info);
 
-    char input[512];
-    while (1) {
-        if (!fgets(input, 512, stdin)) {
-            if (ferror(stdin)) {
-                perror("fgets");
-                stop_thread(elptid);
-                return 1;
-            }
-
-            break;
-        }
-
-        elp_give_msg(&info, input);
-    }
-
+    int ret = stdin_loop(&info);   
+ 
     printf("Shutting down\n");
     stop_thread(elptid);
     elpinfo_dtry(&info);
 
-    return 0;
+    return ret;
 }
